0c50a84a434d7499774dd7f3d36344ad01e2f188
mabagoury <wnajsksos@gmail.com>
2024-10-12 15:59:05 +0300

    mount swagger on app

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import fs from 'fs'
+import YAML from 'yaml';
+import * as swaggerUi from 'swagger-ui-express';
 
 import express from 'express';
 import bodyParser from "body-parser";
@@ -18,6 +21,14 @@ tasks.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 tasks.use('/tasks', isAuthenticated,tasksRouter);
 tasks.use('/users', usersRouter);
+
+// Setup Swagger docs
+const swaggerFile = fs.readFileSync(process.cwd() + '/swagger.yaml', 'utf8');
+const swaggerDocument = YAML.parse(swaggerFile);
+const swaggerUiOptions = {
+  customSiteTitle: 'Tasks',
+};
+tasks.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerUiOptions));
 
 const PORT = process.env.PORT || 3000;
 
